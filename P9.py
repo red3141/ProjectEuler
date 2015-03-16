6d4314432473cbcfaf9960e69e86efe534956e1c
red3141 <red3141@gmail.com>
2015-03-15 21:04:35 -0400

    P9
    P9 completed.

---
+++ P9.py
@@ -0,0 +1,9 @@
+def P9():
+  for i in range(1, 1000):
+    for j in range(1, 1000 - i):
+      k = 1000 - i - j
+      if (i ** 2 + j ** 2) == k ** 2:
+        return i * j * k
+
+if __name__ == "__main__":
+  print P9()
